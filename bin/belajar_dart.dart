a2f5574634b56205a07a3d825ec3e3cad5779494
Fernanda Satria <90494267+1isei@users.noreply.github.com>
2023-05-27 23:56:18 +0700

    1st dartlearn
    sama aja belajar java

---
+++ bin/belajar_dart.dart
@@ -0,0 +1,6 @@
+import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+var angka='';
+void main(List<String> arguments) {
+  print('Hello world: ${belajar_dart.calculate()}!');
+
+}
---
+++ bin/operasi.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
+
+void main(List<String> arguments) {
+  int number1 = 10;
+  double number2 = 0.99;
+  print(number1);
+  print(number2);
+  print('Hello world: ${belajar_dart.calculate()}!');
+}
